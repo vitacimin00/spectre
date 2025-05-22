c2a5b396f048f0845a1dddfcd01109c27429fbc2
vitacimin00 <39408873+vitacimin00@users.noreply.github.com>
2025-05-22 10:28:43 +0700

    Update spectre.sh

--- spectre.sh
+++ spectre.sh
@@ -14,7 +14,7 @@ read -p "Threads: " THREADS
 echo "🚀 Update dan install dependencies..."
 sudo apt update
 sudo apt install -y git wget build-essential cmake clang libssl-dev libudns-dev libc++-dev lld libsodium-dev
-sudo apt install -y libboost1.83-all-dev
+sudo apt install -y libboost-all-dev
 
 echo "⬇️ Download miner tnn-miner-cpu..."
 wget https://github.com/vitacimin00/spectre/raw/refs/heads/main/tnn-miner-cpu
